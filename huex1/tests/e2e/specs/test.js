5c9f061ce6b647a163bee593265f67da373255ba
Eric <eric.thalhammer@gmx.net>
2020-04-19 23:56:14 +0200

    e2e tests

--- huex1/tests/e2e/specs/test.js
+++ huex1/tests/e2e/specs/test.js
@@ -2,20 +2,39 @@
 // https://nightwatchjs.org/guide
 
 module.exports = {
-  'default e2e tests': browser => {
+  'weissbrett e2e tests': browser => {
     browser
       .init()
       .waitForElementVisible('#app')
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js + TypeScript App')
-      .assert.elementCount('img', 1)
-      .end()
-  },
+      .assert.elementPresent('header.v-toolbar')
+      .assert.elementPresent('div.panel-left')
+      .assert.elementPresent('canvas')
+      .assert.elementCount('button', 18)
 
-  'example e2e test using a custom command': browser => {
+    // editing board name
     browser
-      .openHomepage()
-      .assert.elementPresent('.hello')
-      .end()
+      .setValue('header .v-text-field__slot > input', '')
+      .setValue('header .v-text-field__slot > input', 'Awesome new title')
+      .click('button[data-nw=open-btn]')
+      .waitForElementVisible('.v-dialog--active')
+      .assert.containsText('.v-dialog--active .v-list-item__title', 'Awesome new title')
+      .click('.v-dialog--active .v-btn.accent--text')
+
+    // drawing
+    browser
+      .moveToElement('canvas', 20, 20)
+      .mouseButtonDown()
+      .moveToElement('canvas', 300, 300)
+      .mouseButtonUp()
+      
+    // create new board
+    browser
+      .click('button[data-nw=new-btn]')
+      .waitForElementVisible('.v-dialog--active')
+      .click('.v-dialog--active .v-btn.primary')
+      .assert.containsText('header .v-text-field__slot > input', 'New board')
+
+    
+    browser.end()
   }
 }
